e041c3cdf4154a3ab7fc0c4d7fcce7aff9a9168d
Arafat <abdukarimovarafat@gmail.com>
2025-07-25 12:00:25 +0500

    eror

---
+++ eror.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type User struct {
+	FirstName string
+	LastName  string
+	Age       int
+}
+
+func CreateUser(firstName, lastName string, age int) (*User, error) {
+	// Проверка имени
+	if firstName == "" {
+		return nil, errors.New("имя не может быть пустым")
+	}
+
+	if lastName == "" {
+		return nil, errors.New("фамилия не может быть пустой")
+	}
+
+	if age <= 18 {
+		return nil, errors.New("пользователь должен быть старше 18 лет")
+	}
+
+	user := &User{
+		FirstName: firstName,
+		LastName:  lastName,
+		Age:       age,
+	}
+
+	return user, nil
+}
+
+func main() {
+
+	user, err := CreateUser("Arafat", "Abdukarimov", 25)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Пользователь [%s %s] успешно создан\n", user.FirstName, user.LastName)
+
+	_, err2 := CreateUser("Pupkina", "Zalu", 16)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module eror.go
+
+go 1.24.3
